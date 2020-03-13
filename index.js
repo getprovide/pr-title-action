3e214829e9dc65d0b7852c25663d3ea5f439e890
Tim Wood <washwithcare@gmail.com>
2020-03-13 13:26:35 -0500

    Add event payload for debugging

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ const payload = github.context.payload
 const title = payload && payload.pull_request && payload.pull_request.title
 
 core.info(title)
+core.info(`The event payload: ${JSON.stringify(payload, undefined, 2)}`)
 
-if (!pattern.test(title)) core.setFailed(`Pull request title "${title}" does not contain a JIRA issue key.`,)
+if (!pattern.test(title)) core.setFailed(`Pull request title "${title}" does not contain a JIRA issue key.`)
